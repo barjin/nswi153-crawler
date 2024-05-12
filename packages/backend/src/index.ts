57730065308ec5d2a425b51b85ecf7f8e61831b5
Jindřich Bär <jindrichbar@gmail.com>
2024-05-12 09:38:18 +0200

    feat: add mock backend implementation

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,43 +1,123 @@
-import express from 'express';
-import getopts from 'getopts';
+import express from 'express'
+import getopts from 'getopts'
+import { components } from 'packages/openapi-specification/lib/api-types';
 
 const app = express();
 
-/* Execution */
-app.delete('/execution/:executionId', (req, res) => {
-    // TODO
-    res.status(503).send('Unimplemented');
-});
-app.get('/execution', (req, res) => {
-    // TODO
-    res.status(503).send('Unimplemented');
-});
-app.get('/execution/:executionId', (req, res) => {
-    // TODO
-    res.status(503).send('Unimplemented');
-});
+let executions: components['schemas']['Execution'][] = [
+	{
+		id: 1,
+		startURL: 'https://example.com',
+		nodes: [
+			{
+				url: 'https://example.com',
+				links: [
+					3,
+					4,
+				],
+				crawlTime: new Date().toISOString(),
+				title: 'Example Domain',
+			},
+		],
+	},
+	{
+		id: 2,
+		startURL: 'https://wikipedia.org',
+		nodes: [
+			{
+				url: 'https://wikipedia.org',
+				links: [
+					5,
+					6,
+				],
+				crawlTime: new Date(Date.now() - 10e2).toISOString(),
+				title: 'Wikipedia',
+			},
+		]
+	},
+];
 
-/* WebsiteRecord */
-app.post('/records', (req, res) => {
-    // TODO
-    res.status(503).send('Unimplemented');
-});
-app.delete('/records/:recordId', (req, res) => {
-    // TODO
-    res.status(503).send('Unimplemented');
-});
-app.get('/records/:recordId', (req, res) => {
-    // TODO
-    res.status(503).send('Unimplemented');
-});
-app.get('/records', (req, res) => {
-    // TODO
-    res.status(503).send('Unimplemented');
-});
-app.put('/records/:recordId', (req, res) => {
-    // TODO
-    res.status(503).send('Unimplemented');
-});
+/* Execution */
+app.delete('/execution/:executionId', function(req, res) {
+	const executionId = parseInt(req.params.executionId)
+	executions = executions.filter(({ id }) => id !== executionId);
+
+	return res.status(204).send()
+})
+
+app.get('/execution', function(req, res) {
+	return res.json(executions)
+})
+app.get('/execution/:executionId', function(req, res) {
+	const executionId = parseInt(req.params.executionId)
+	const execution = executions.find(({ id }) => id === executionId)
+
+	if (!execution) {
+		return res.status(404).send()
+	}
+
+	return res.json(execution)
+})
+
+
+let websiteRecords: components['schemas']['WebsiteRecord'][] = [
+	{
+		id: 1,
+		url: 'https://example.com',
+		boundaryRegEx: '^https://example.com/.*',
+		isActive: true,
+		periodicity: 3600,
+		label: 'Example Domain',
+	},
+	{
+		id: 2,
+		url: 'https://cs.wikipedia.org',
+		boundaryRegEx: '^https://cs.wikipedia.org/wiki/.*',
+		isActive: false,
+		periodicity: 86400,
+		label: 'Czech Wikipedia | scraping disabled',
+	},
+]
+
+app.post('/records', function(req, res) {
+	const record = req.body as components['schemas']['WebsiteRecord']
+	const recordId = websiteRecords.push(record) - 1
+
+	return res.status(201).json({ id: recordId })
+})
+app.delete('/records/:recordId', function(req, res) {
+	const recordId = parseInt(req.params.recordId)
+	websiteRecords = websiteRecords.filter(({ id }) => id !== recordId);
+
+	return res.status(204).send()
+})
+app.get('/records/:recordId', function(req, res) {
+	const recordId = parseInt(req.params.recordId)
+	const record = websiteRecords.find(({ id }) => id === recordId)
+
+	if (!record) {
+		return res.status(404).send()
+	}
+
+	return res.json(record)
+})
+app.get('/records', function(req, res) {
+	return res.json(websiteRecords)
+})
+app.put('/records/:recordId', function(req, res) {
+	const recordId = parseInt(req.params.recordId)
+	const record = req.body as components['schemas']['WebsiteRecord']
+
+	websiteRecords.find((record, index) => {
+		if (record.id === recordId) {
+			websiteRecords[index] = {...websiteRecords[index], ...record}
+			return true
+		}
+		return false
+	});
+
+	return res.status(204).send()
+})
 
 const options = getopts(process.argv.slice(2), {
     alias: {
