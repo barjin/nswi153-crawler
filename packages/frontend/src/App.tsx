dfdd64184cf40d5644fd1a158dfe0ca95b226b1e
Jindřich Bär <jindrichbar@gmail.com>
2024-05-19 08:38:18 +0200

    feat: initial frontend implementation

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,35 +1,93 @@
 import { paths } from '@nswi153-crawler/openapi-spec/lib/api-types';
 import createClient from 'openapi-fetch';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const api = createClient<paths>({
-    baseUrl: 'http://localhost:8080/api',
+    baseUrl: 'http://localhost:3000/',
 });
 
-function App() {
-    // Example usage of the generated client - note that the API is fully typed, so you get autocompletion and type checking
-    api.GET('/execution/{executionId}', {
-        params: {
-            path: {
-                executionId: 123,
-            },
-        },
-    }).then(() => {}).catch(() => {});
+type WebsiteRecord = paths['/records']['get']['responses']['200']['content']['application/json'][number];
 
-    const [count, setCount] = useState(0);
+function RecordRow({ record }: { record: WebsiteRecord }) {
+    return (
+        <tr
+            key={record.id}
+            className={`
+        even:bg-slate-100 
+        odd:bg-slate-50 
+        cursor-pointer
+        hover:bg-slate-200
+      `}
+        >
+            <td>{record.isActive ? '✅' : '❌'}</td>
+            <td>
+                <div className='flex flex-col py-2'>
+                    <span>{record.label}</span>
+                    <span className='text-xs'>
+                        <span className='font-semibold'>
+                            {record.url}
+                        </span>&nbsp;|&nbsp;
+                        <span className='text-gray-500'>
+                            { record.boundaryRegEx }
+                        </span>
+                    </span>
+                </div>
+            </td>
+        </tr>
+    );
+}
+
+function App() {
+    const [records, setRecords] = useState<WebsiteRecord[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        api.GET('/records').then((response) => {
+            setRecords(response.data ?? []);
+            setLoading(false);
+        });
+    }, []);
 
     return (
         <>
-            <h1>NSWI153 Crawler</h1>
-            <p>
-        So far, there is nothing much here.
-            </p>
-            <div className="card">
-                <button onClick={() => setCount(() => count + 1)}>
-          count is {count}
-                </button>
+            <div
+                className='grid grid-cols-3'
+            >
+                <div
+                    className='col-start-2 col-span-1 h-screen border-r border-slate-100 shadow-md rounded-lg p-4'
+                >
+                    <h1>Website Records</h1>
+                    <table
+                        className={`
+          bg-slate-50
+          w-full
+
+        `}
+                    >
+                        <thead
+                            className={`
+              bg-slate-100
+              text-left
+              text-black
+              text-sm
+              font-semibold
+            `}
+                        >
+                            <tr>
+                                <th>Active</th>
+                                <th>Record</th>
+                            </tr>
+                        </thead>
+                        <tbody
+                            className='text-sm text-left text-black'
+                        >
+                            {loading ? <tr><td>Loading...</td></tr> : records.map((record) => (
+                                <RecordRow record={record} />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     );
