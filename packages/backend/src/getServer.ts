b415d05b70f1a72bbafbf79414b7bb89960b2482
Jindřich Bär <jindrichbar@gmail.com>
2024-09-01 22:23:25 +0200

    chore: add `cors()` middleware instead of manual header handling

--- packages/backend/src/getServer.ts
+++ packages/backend/src/getServer.ts
@@ -1,6 +1,7 @@
 import path from "path";
 
 import bodyParser from "body-parser";
+import cors from "cors";
 import express, { Router } from "express";
 
 import "reflect-metadata";
@@ -9,16 +10,11 @@ import { getGraphQlRouter } from "./graphql/graphql";
 import { getExecutionsRouter } from "./routes/executions";
 import { getRecordsRouter } from "./routes/records";
 
+
 export async function getServer() {
   const app = express();
 
-  app.all("*", (req, res, next) => {
-    console.log(`Serving ${req.method} ${req.path}`);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    next();
-  });
+  app.use(cors());
 
   app.use(bodyParser.json());
 
